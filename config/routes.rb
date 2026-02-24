8040ff5b1b68e9b5293bbc77aabedb87b5feed46
Ma-75K <k.mafuyu.0124@gmail.com>
2026-02-24 12:56:57 +0900

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: proc { [200, {}, ['Welcome to Kotonoha!']] }
+  get "up" => "rails/health#show", as: :rails_health_check
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
