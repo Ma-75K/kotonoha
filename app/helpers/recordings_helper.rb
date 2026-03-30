39bab6e42c4f339e8b2193efb7c5c060bfadedb4
Ma-75K <k.mafuyu.0124@gmail.com>
2026-03-30 10:29:03 +0900

    index.html.erbの実装

--- app/helpers/recordings_helper.rb
+++ app/helpers/recordings_helper.rb
@@ -1,2 +1,10 @@
 module RecordingsHelper
+  def format_duration(seconds)
+    return "00:00" if seconds.blank?
+
+    minutes = seconds / 60
+    remaining_seconds = seconds % 60
+
+    format("%02d:%02d", minutes, remaining_seconds)
+  end
 end
