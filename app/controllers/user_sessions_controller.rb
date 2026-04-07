c3d5650d069292c2ba7675e29b567a0dc4dbbd6b
Ma-75K <k.mafuyu.0124@gmail.com>
2026-04-07 11:56:56 +0900

    ログイン処理と子ども登録の一部修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,14 +7,8 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      child = @user.children.first
-
-      if child
-        session[:current_child_id] = child.id
-        redirect_to new_child_recording_path(child), success: "ログインしました"
-      else
-        redirect_to new_child_path, alert: "お子さまを登録してください"
-      end
+      session[:current_child_id] = @user.children.first.id unless session[:current_child_id]
+      redirect_to new_child_recording_path(current_child), success: "ログインしました"
     else
       flash.now[:alert] = "メールアドレスまたはパスワードが正しくありません"
       render :new, status: :unprocessable_entity
