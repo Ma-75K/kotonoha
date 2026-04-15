eef911f418a0c5d79740d218c0be34d9e757497c
Ma-75K <k.mafuyu.0124@gmail.com>
2026-04-15 23:26:31 +0900

    spec/models/child_spec.rb を書く

--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Child, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "name、birthday、user があれば有効" do
+      child = build(:child)
+      expect(child).to be_valid
+    end
+
+    it "name がない場合は無効" do
+      child = build(:child, name: nil)
+      expect(child).to be_invalid
+      expect(child.errors[:name]).to include("を入力してください")
+    end
+
+    it "birthday がない場合は無効" do
+      child = build(:child, birthday: nil)
+      expect(child).to be_invalid
+      expect(child.errors[:birthday]).to include("を入力してください")
+    end
+
+    it "user に紐付いていること" do
+      child = build(:child)
+      expect(child.user).to be_present
+    end
+  end
 end
