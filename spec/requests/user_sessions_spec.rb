edb55c9edb86d7e82682b2677796b2bfa81cd4a0
Ma-75K <k.mafuyu.0124@gmail.com>
2026-04-18 23:46:48 +0900

    認証・ログイン周りのリクエストテストの実装

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -1,24 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/user_sessions/new"
-      expect(response).to have_http_status(:success)
+  describe "POST /login" do
+    it "正しい情報でログインできる" do
+      user = create(:user, password: "password", password_confirmation: "password")
+      child = create(:child, user: user)
+
+      post login_path, params: {
+        email: user.email,
+        password: "password"
+      }
+
+      expect(response).to redirect_to(new_child_recording_path(child))
+    end
+
+    it "間違った情報ではログインできない" do
+      user = create(:user, password: "password", password_confirmation: "password")
+
+      post login_path, params: {
+        email: user.email,
+        password: "wrong_password"
+      }
+
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/user_sessions/create"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /logout" do
+    it "ログアウトできる" do
+      user = create(:user, password: "password", password_confirmation: "password")
+      child = create(:child, user: user)
+
+      post login_path, params: {
+        email: user.email,
+        password: "password"
+      }
+
+      delete logout_path
+
+      expect(response).to redirect_to(root_path)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/user_sessions/destroy"
-      expect(response).to have_http_status(:success)
+  describe "認証が必要なページ" do
+    it "未ログインで設定画面にアクセスするとログイン画面にリダイレクトする"  do
+      get settings_path
+
+      expect(response).to redirect_to(login_path)
     end
   end
 end
