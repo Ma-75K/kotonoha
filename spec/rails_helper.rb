42fac20af0a1022240537f932e2457b437b376fd
Ma-75K <k.mafuyu.0124@gmail.com>
2026-04-21 09:53:08 +0900

    system specの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
+
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -40,6 +42,8 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+
+  config.infer_spec_type_from_file_location!
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
---
+++ spec/system/main_flow_spec.rb
@@ -0,0 +1,92 @@
+require "rails_helper"
+
+RSpec.describe "Main Flow", type: :sysytem do
+  include Capybara::DSL
+  include Rails.application.routes.url_helpers
+
+  it "ログインしてホーム画面に遷移できる" do
+    user = create(:user, password: "password", password_confirmation: "password")
+    child = create(:child, user: user, name: "さき")
+
+    visit login_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+
+    click_button "ログイン"
+
+    expect(page).to have_current_path(new_child_recording_path(child))
+    expect(page).to have_content("録音をはじめる")
+  end
+
+  it "設定画面に移動できる" do
+    user = create(:user, password: "password", password_confirmation: "password")
+    child = create(:child, user: user)
+
+    visit login_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
+
+    visit settings_path
+
+    expect(page).to have_content("お子さま情報設定")
+  end
+
+  it "子どもを追加できる" do
+    user = create(:user, password: "password", password_confirmation: "password")
+    child = create(:child, user: user)
+
+    visit login_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
+
+    visit new_from_settings_children_path
+
+    fill_in "名前", with: "みなと"
+    fill_in "生年月日", with: "2023-01-01"
+
+    click_button "追加"
+
+    expect(page).to have_current_path(settings_path)
+    expect(page).to have_content("みなと")
+  end
+
+  it "子どもを編集できる" do
+    user = create(:user, password: "password", password_confirmation: "password")
+    child = create(:child, user: user)
+
+    visit login_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
+
+    visit edit_child_path(child)
+
+    fill_in "名前", with: "さきちゃん"
+    click_button "更新"
+
+    expect(page).to have_content("さきちゃん")
+  end
+
+  it "主要画面遷移ができる" do
+    user = create(:user, password: "password", password_confirmation: "password")
+    child = create(:child, user: user)
+
+    visit login_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
+
+    visit child_recordings_path(child)
+    expect(page).to have_current_path(child_recordings_path(child))
+
+    visit on_this_day_child_recordings_path(child)
+    expect(page).to have_current_path(on_this_day_child_recordings_path(child))
+  end
+end
