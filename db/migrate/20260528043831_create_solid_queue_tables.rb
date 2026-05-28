b0218a1767275eeae9008b845620e12dcf346d10
Ma-75K <k.mafuyu.0124@gmail.com>
2026-05-28 14:24:54 +0900

    自動修正

--- db/migrate/20260528043831_create_solid_queue_tables.rb
+++ db/migrate/20260528043831_create_solid_queue_tables.rb
@@ -12,11 +12,11 @@ class CreateSolidQueueTables < ActiveRecord::Migration[8.0]
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
 
-      t.index ["active_job_id"], name: "index_solid_queue_jobs_on_active_job_id"
-      t.index ["class_name"], name: "index_solid_queue_jobs_on_class_name"
-      t.index ["finished_at"], name: "index_solid_queue_jobs_on_finished_at"
-      t.index ["queue_name", "finished_at"], name: "index_solid_queue_jobs_for_filtering"
-      t.index ["scheduled_at", "finished_at"], name: "index_solid_queue_jobs_for_alerting"
+      t.index [ "active_job_id" ], name: "index_solid_queue_jobs_on_active_job_id"
+      t.index [ "class_name" ], name: "index_solid_queue_jobs_on_class_name"
+      t.index [ "finished_at" ], name: "index_solid_queue_jobs_on_finished_at"
+      t.index [ "queue_name", "finished_at" ], name: "index_solid_queue_jobs_for_filtering"
+      t.index [ "scheduled_at", "finished_at" ], name: "index_solid_queue_jobs_for_alerting"
     end
 
     create_table "solid_queue_blocked_executions" do |t|
