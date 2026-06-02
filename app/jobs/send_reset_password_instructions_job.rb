0380e44b9aba839cbe6887bf7774af6b9e25f2f3
Ma-75K <k.mafuyu.0124@gmail.com>
2026-06-02 21:31:20 +0900

    修正

--- app/jobs/send_reset_password_instructions_job.rb
+++ app/jobs/send_reset_password_instructions_job.rb
@@ -3,6 +3,8 @@ class SendResetPasswordInstructionsJob < ApplicationJob
 
   def perform(user_id)
     user = User.find(user_id)
-    user.deliver_reset_password_instructions!
+
+    user.generate_reset_password_token!
+    UserMailer.reset_password_email(user).deliver_now
   end
 end
