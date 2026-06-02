d767ee8cd2dc8df701b6a5bafa391462f4f8007b
Ma-75K <k.mafuyu.0124@gmail.com>
2026-06-02 09:08:18 +0900

    Config Active Storage to use S3 in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,9 +24,6 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
 
@@ -100,6 +97,5 @@ Rails.application.configure do
 
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  # Active Storageの設定
-  config.active_storage.service = :production
+  config.active_storage.service = :amazon
 end
